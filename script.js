798f728e56386e735c761d71a05e3cb564f383c2
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2023-02-18 00:22:02 +0300

    added burger menu for mobiles

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   $('.menu_burger').click(function() {
+       $('.header_nav_elements').toggleClass('open-menu');
+       $('body').toggleClass('open-menu');
+   });
+
+   $('.closesign').click(function() {
+      $('.header_nav_elements').removeClass('open-menu');
+      $('body').removeClass('open-menu');
+  });
+});
