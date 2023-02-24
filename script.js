b3a5a2ed781dabc57738c34cb936f1904ca3821b
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2023-02-24 17:03:46 +0300

    fixed bug with scroll

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
    $('.menu_burger').click(function() {
        $('.header_nav_elements').toggleClass('open-menu');
        $('body').toggleClass('open-menu');
+       $('html').toggleClass('open-menu');
    });
 
    $('.closesign').click(function() {
       $('.header_nav_elements').removeClass('open-menu');
       $('body').removeClass('open-menu');
+      $('html').removeClass('open-menu');
   });
 });
