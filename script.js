3ae9cbd34ef869f27e1795b63feff0994599ce9e
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2023-04-07 17:54:35 +0300

    last update of all versions
    made test section on js, fixed bugs, fixed adaptive,will look for some bugs

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+   var numberofcorrect = 0;
+
    $('.menu_burger').click(function() {
        $('.header_nav_elements').addClass('open-menu');
        $('body').addClass('open-menu');
@@ -23,11 +26,37 @@ $(document).ready(function() {
    });
 
    $('.closepopap').click(function() {
-      $('.standartwindow').removeClass('inactive');
-      $('.question1').addClass('inactive');
       $('body').removeClass('open-menu');
       $('html').removeClass('open-menu');
       $('.popapquest').removeClass('open-menu');
+      $('.standartwindow').removeClass('inactive');
+      $('.question1').addClass('inactive');
+      $('.question1-answer').addClass('inactive');
+      $('.answer-header2').addClass('inactive');
+      $('.answer-header1').removeClass('inactive');
+      $('.result-of-test').addClass('inactive');
+      if (document.querySelector('input[name="question1"]:checked'))
+      {
+         document.querySelector('input[name="question1"]:checked').checked = false;
+      }
+      numberofcorrect = 0
+   });
+
+   $('.result__button').click(function() {
+      $('body').removeClass('open-menu');
+      $('html').removeClass('open-menu');
+      $('.popapquest').removeClass('open-menu');
+      $('.standartwindow').removeClass('inactive');
+      $('.question1').addClass('inactive');
+      $('.question1-answer').addClass('inactive');
+      $('.answer-header2').addClass('inactive');
+      $('.answer-header1').removeClass('inactive');
+      $('.result-of-test').addClass('inactive');
+      if (document.querySelector('input[name="question1"]:checked'))
+      {
+         document.querySelector('input[name="question1"]:checked').checked = false;
+      }
+      numberofcorrect = 0
    });
 
    $('.popapbut').click(function() {
@@ -35,4 +64,32 @@ $(document).ready(function() {
       $('.question1').removeClass('inactive');
    });
 
+   $('.question1__button').click(function() {
+      let checked = document.querySelector('input[name="question1"]:checked');
+      if (checked)
+      {
+         $('.question1').addClass('inactive');
+         $('.question1-answer').removeClass('inactive');
+         if (checked.value != "correct")
+         {
+            $('.answer-header1').addClass('inactive');
+            $('.answer-header2').removeClass('inactive');
+         }
+         else
+         {
+            ++numberofcorrect;
+         }
+      }
+      else
+      {
+         alert('Вы не выбрали вариант ответа');
+      }
+   });
+
+   $('.answer1__button').click(function() {
+      var str = numberofcorrect.toString();
+      $('.question1-answer').addClass('inactive');
+      $('.result-of-test').removeClass('inactive');
+      $('.my__result').text(str);
+   });
 });
